c6d72007086e3d47e0d6dc1535a851b9ad38a668
Francisco Montoto <fmontotomonroy@gmail.com>
2015-09-13 16:13:37 -0300

    Move dtc_get_error_msg to a c file

--- cryptoki/dt_tclib/err.h
+++ cryptoki/dt_tclib/err.h
@@ -16,27 +16,4 @@ enum {
     DTC_ERR_MAX_ // Keep at the end!!
 };
 
-const char *err_msgs[] = {
-    "Not an error.",
-    "Not enough memory.",
-    "Error reading config file.",
-    "Error at zmq library.",
-    "Error setting zmq curve security.",
-    "Error in the socket connection.",
-    "Communication error.",
-    "Error in the serialization.",
-    "Sqlite error.",
-    "Intern library error.",
-
-    "Wrong errno.",  // Keep at the end.
-};
-
-const char *dtc_get_error_msg(int errno) {
-
-    if(errno >= DTC_ERR_MAX_ || errno < 0)
-        return err_msgs[DTC_ERR_MAX_];
-
-    return err_msgs[errno];
-}
-
 #endif
