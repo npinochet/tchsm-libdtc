c31656be5e40ce3e99d9be3e3eb218baf75aac06
fmontoto <fmontotomonroy@gmail.com>
2015-11-09 11:01:01 -0300

    Refactor some common functions into utilities

--- cryptoki/dt_tclib/src/utilities.h
+++ cryptoki/dt_tclib/src/utilities.h
@@ -3,6 +3,15 @@
 
 #include <libconfig.h>
 
+#ifndef NDEBUG
+    #include "logger/logger.h"
+    #define LOG_DEBUG(level, format, ...) \
+        LOG(level, format, ## __VA_ARGS__)
+#else
+    #define LOG_DEBUG(level, format, ...) \
+        do {}while(0);
+#endif
+
 /**
  * Auxiliary function to read an uint16_t from a config_setting_t, it's read
  * as int64 and then tested if it fit in an uint16_t.
@@ -39,5 +48,46 @@ int lookup_string_conf_element(const config_setting_t *setting,
  * @return ret
  */
 char *get_uuid_as_char(char *ret);
+
+/**
+ * Call free_func with data as arg.
+ *
+ * @param data Data to be freed.
+ * @param free_func Functio to be called in order to free data.
+ */
+void free_wrapper(void *data, void *hint);
+
+/**
+ * Send a string as zmq msg.
+ *
+ * @param socket ZMQ socket to send the message through.
+ * @param string C String to be send as message.
+ *
+ * @return Bytes sent.
+ */
+int s_send(void *socket, const char *string);
+
+/**
+ * Send a string as not terminal zmq multipart message.
+ *
+ * @param socket ZMQ socket to send the message through.
+ * @param string C String to be send as message.
+ *
+ * @return Bytes sent.
+ */
+int s_sendmore(void *socket, const char *string);
+
+/**
+ * Receive a ZMQ string from a zmq_socket and convert it into a C string The
+ * function will receive a string with a max len of 255. The string will be
+ * truncated if it's larger than 255.
+ *
+ * @param socket socket to receive from.
+ *
+ * @return The received string on success or NULL on error. The caller is
+ *      responsible for the memory on success and should call free to avoid
+ *      memory leaks.
+ */
+char *s_recv(void *socket);
 
 #endif // DT_TCLIB_UTILTIES_H_
