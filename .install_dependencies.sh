53255f19159106dc2aec73403df555dd4b6037f6
fmontoto <fmontotomonroy@gmail.com>
2016-04-04 17:12:14 -0300

    Add botan as dependency in continuous integration

--- .install_dependencies.sh
+++ .install_dependencies.sh
@@ -15,6 +15,9 @@ sudo tar -xzC /opt -f libsodium-1.0.2.tar.gz
 wget http://download.zeromq.org/zeromq-4.1.4.tar.gz
 sudo tar -xzC /opt -f zeromq-4.1.4.tar.gz
 
+wget http://botan.randombit.net/releases/Botan-1.11.29.tgz
+sudo tar -xzC /opt -f Botan-1.11.29.tgz
+
 wget https://github.com/niclabs/tclib/archive/master.zip
 sudo unzip master.zip -d /opt
 
@@ -23,3 +26,5 @@ cd /opt/libsodium-1.0.2/ && sudo ./configure && sudo make && sudo make install
 cd /opt/zeromq-4.1.4 && sudo ./configure --with-libsodium && sudo make && sudo make install
 
 cd /opt/tclib-master && sudo mkdir build && cd build && sudo cmake .. && sudo make install
+
+cd /opt/Botan-1.11.29 && ./configure && make && sudo make install
