80d9e470fb87c53cf7cf1cca68b4c66cf0118a1e
DanielAviv <daniel_avivnotario@hotmail.com>
2016-05-16 15:21:27 -0400

    Initial commit, previous system test files were moved, necesary folders were created, empty testing script was also created.

---
+++ tests/system-test/system_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+"""
+This module creates the configuration files of nodes and masters for testing purposes.
+"""
+
+__author__ = "Daniel Aviv"
+__email__ = "daniel_avivnotario@hotmail.com"
+__credits__ = ["Francisco Montoto", "Francisco Cifuentes"]
+__status__ = "Development"
+
+DUMP = "../test-dump"
+
+def erase_dump():
+	
+
+def main(argv=None):
+	return 0
+
+if __name__ == "__main__":
+	main(sys.argv)
