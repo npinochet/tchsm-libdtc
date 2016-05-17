724a54c9416bf7b409c8f167a8701adad6e5e555
DanielAviv <daniel_avivnotario@hotmail.com>
2016-05-17 17:33:32 -0400

    Managed to "kind of" test the behaviour when starting up a node, this doesn't control a timeout, and I suspect that the process doesn't end well if the test doesn't pass.

--- tests/system-test/system_test.py
+++ tests/system-test/system_test.py
@@ -5,6 +5,10 @@ import sys
 from os.path import exists
 from tempfile import mkdtemp
 import shutil
+from commands import getstatusoutput
+import subprocess
+from threading import Timer
+from shlex import split
 
 """
 
@@ -17,6 +21,10 @@ __status__ = "Development"
 
 DUMP = ""
 
+NODE_RDY = "Both socket binded, node ready to talk with the Master."
+
+TEST_TIMEOUT = 10
+
 
 def erase_dump():
     if exists(DUMP):
@@ -25,14 +33,32 @@ def erase_dump():
 
 
 def test_one_node():
-    pass
+    getstatusoutput("python ../../scripts/create_config.py 127.0.0.1:3001:3002 -o " + DUMP)
+
+    proc = subprocess.Popen(["/home/danielaviv/install/bin/node", "-c", DUMP + "/node1.conf"], stderr=subprocess.PIPE)
+    timer = Timer(TEST_TIMEOUT, proc.kill)
+
+    stdout_lines = iter(proc.stderr.readline, "")
+    for stdout_line in stdout_lines:
+        if NODE_RDY in stdout_line:
+            proc.stderr.close()
+            proc.terminate()
+            return 0
+
+    proc.stderr.close()
+    proc.terminate()
+    return 1
+
+
+def test_two_nodes():
+    return 1
 
 
 def main(argv=None):
     global DUMP
-    DUMP = mkdtemp(dir="../")
+    DUMP = mkdtemp(dir="./")
 
-    test_one_node()
+    print "OUTPUT: " + str(test_one_node())
 
     erase_dump()
     return 0
