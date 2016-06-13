002f0a8ab0d148890b76260fea3177c1fa7d4829
DanielAviv <daniel_avivnotario@hotmail.com>
2016-06-13 14:34:18 -0400

    Basic initialization

--- tests/system_test/pkcs_11_test.py
+++ tests/system_test/pkcs_11_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import PyKCS11
 import sys
 
 """
@@ -13,7 +14,41 @@ __credits__ = ["Francisco Montoto", "Francisco Cifuentes"]
 __status__ = "Development"
 
 
+DEFAULT_PIN = 1234
+
+
+def initialize(lib):
+    pkcs11 = PyKCS11.PyKCS11Lib()
+    pkcs11.load(lib)
+
+
 def main(argv=None):
+    import argparse
+
+    parser = argparse.ArgumentParser(description="System Testing")
+    parser.add_argument("-c",
+                        "--create_key",
+                        help="",
+                        default=False,
+                        action="store_true")
+    parser.add_argument("-f",
+                        "--filename",
+                        help="",
+                        type=str)
+    parser.add_argument("-n",
+                        "--amount_of_sign",
+                        help="",
+                        type=int)
+    parser.add_argument("-p",
+                        "--pin",
+                        help="",
+                        default=DEFAULT_PIN,
+                        type=int)
+    args = parser.parse_args()
+
+    lib = None
+    initialize(lib)
+
     print "Sekai-san, konnichiwa"
     return 0
 
