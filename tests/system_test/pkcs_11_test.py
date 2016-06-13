a92c142cfe890d6c27e86def14331cd127ace7af
DanielAviv <daniel_avivnotario@hotmail.com>
2016-06-13 11:50:20 -0400

    First commit

---
+++ tests/system_test/pkcs_11_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+"""
+
+"""
+
+__author__ = "Daniel Aviv"
+__email__ = "daniel_avivnotario@hotmail.com"
+__credits__ = ["Francisco Montoto", "Francisco Cifuentes"]
+__status__ = "Development"
+
+
+def main(argv=None):
+    print "Sekai-san, konnichiwa"
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
