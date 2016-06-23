7d4450d1336cf075d28fe7ffaa74fcf555ec701b
Francisco Cifuentes <francisco@niclabs.cl>
2016-06-23 15:08:25 -0400

    Hotfix: use Botan1.10; ldconfig after zmq install

--- .install_dependencies.sh
+++ .install_dependencies.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y libjson-c-dev
 sudo apt-get install -y libconfig-dev
 sudo apt-get install -y libsqlite3-dev
 sudo apt-get install -y uuid-dev
-
+sudo apt-get install -y libbotan1.10-dev
 
 wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.9.tar.gz
 tar -xzC /tmp -f libsodium-1.0.9.tar.gz
@@ -15,20 +15,13 @@ tar -xzC /tmp -f libsodium-1.0.9.tar.gz
 wget https://github.com/zeromq/zeromq4-1/releases/download/v4.1.4/zeromq-4.1.4.tar.gz
 tar -xzC /tmp -f zeromq-4.1.4.tar.gz
 
- #wget http://botan.randombit.net/releases/Botan-1.11.29.tgz
- #tar -xzC /tmp -f Botan-1.11.29.tgz
-
-wget http://200.7.6.140:8080/botan-1.11-ubuntu-14.04.tar.gz
-tar -xzC /tmp -f botan-1.11-ubuntu-14.04.tar.gz
-
 wget https://github.com/niclabs/tchsm-libtc/archive/master.zip
 unzip master.zip -d /tmp
 
 cd /tmp/libsodium-1.0.9/ && ./configure && make && sudo make install
 
-cd /tmp/zeromq-4.1.4 && ./configure --with-libsodium && make && sudo make install
+cd /tmp/zeromq-4.1.4 && ./configure --with-libsodium && make && sudo make install && sudo ldconfig
 
 cd /tmp/tchsm-libtc-master && mkdir build && cd build && cmake .. && sudo make install
 
-#cd /tmp/Botan-1.11.29 && ./configure.py && make && sudo make install
 cd /tmp/botan && sudo rsync -av * /usr/local
