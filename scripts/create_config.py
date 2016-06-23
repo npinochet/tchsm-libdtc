64243d82260bf6551aeccfbd155e9a0ad5619e82
DanielAviv <daniel_avivnotario@hotmail.com>
2016-06-23 14:56:54 -0400

    Changed error message to fix understability (is this a word? I don't think so)

--- scripts/create_config.py
+++ scripts/create_config.py
@@ -201,13 +201,13 @@ def get_keygen():
             stderr=subprocess.PIPE,
             stdout=subprocess.PIPE)
     except OSError:
-        sys.stderr.write("ERROR: Exec could not be accesed >> curve_keygen\n")
+        sys.stderr.write("ERROR: Exec could not be accessed >> curve_keygen\n")
         sys.exit(1)
 
     proc_stdout, proc_stderr = keygen.communicate()
 
     if keygen.returncode != 0:
-        sys.stderr.write("ERROR: curve_keygen found a problem: " + proc_stderr + "\n")
+        sys.stderr.write("ERROR: curve_keygen found a problem: " + proc_stderr.decode() + "\n")
 
         if keygen is not None:
             keygen.stdout.close()
