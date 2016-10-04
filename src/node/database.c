59fc0371ca8e699e7632e3690306aa17420e0969
Francisco Montoto <fmontoto@users.noreply.github.com>
2016-10-04 17:54:51 -0300

    HOTFIX: Add missing ) and remove extra & at database.c

--- src/node/database.c
+++ src/node/database.c
@@ -150,7 +150,7 @@ database_t *db_init_connection(const char *path, int create_db_tables){
 #if (SQLITE_VERSION_NUMBER >= 3007015)
             sqlite3_errstr(rc));
 #else
-            sqlite3_errmsg(&ret->ppDb);
+            sqlite3_errmsg(ret->ppDb));
 #endif
         sqlite3_close(ret->ppDb);
         free(ret);
