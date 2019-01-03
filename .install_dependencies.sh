f7fb54101a09ea01fa1f6a98abe6b5bd4cb59669
lareyes2 <lareyes2@uc.cl>
2019-01-03 12:11:40 -0300

    update dependencies

--- .install_dependencies.sh
+++ .install_dependencies.sh
@@ -13,20 +13,20 @@ sudo apt-get install -y libssl-dev
 
 wget ftp://sourceware.org/pub/libffi/libffi-3.2.1.tar.gz
 tar -xzC /tmp -f libffi-3.2.1.tar.gz && rm libffi-3.2.1.tar.gz
-cd /tmp/libffi-3.2.1 && ./configure && make && make install
+cd /tmp/libffi-3.2.1 && ./configure && make && sudo make install
 
 
-wget https://download.libsodium.org/libsodium/releases/old/unsupported/libsodium-1.0.9.tar.gz
-tar -xzC /tmp -f libsodium-1.0.9.tar.gz
+wget https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz
+tar -xzC /tmp -f libsodium-1.0.16.tar.gz
 
-wget https://github.com/zeromq/zeromq4-1/releases/download/v4.1.4/zeromq-4.1.4.tar.gz
-tar -xzC /tmp -f zeromq-4.1.4.tar.gz
+wget https://github.com/zeromq/zeromq4-1/releases/download/v4.1.6/zeromq-4.1.6.tar.gz
+tar -xzC /tmp -f zeromq-4.1.6.tar.gz
 
 wget https://github.com/niclabs/tchsm-libtc/archive/master.zip
 unzip master.zip -d /tmp
 
-cd /tmp/libsodium-1.0.9/ && ./configure && make && sudo make install
+cd /tmp/libsodium-1.0.16/ && ./configure && make && sudo make install
 
-cd /tmp/zeromq-4.1.4 && ./configure --with-libsodium && make && sudo make install && sudo ldconfig
+cd /tmp/zeromq-4.1.6 && ./configure --with-libsodium && make && sudo make install && sudo ldconfig
 
 cd /tmp/tchsm-libtc-master && ./.install_dependencies.sh && mkdir build && cd build && cmake .. && sudo make install
